26d53ddc6540b5418eb150f443cfd90470814bdb
OmarMeriwani <omar.sirwan@gmail.com>
2019-02-21 20:46:46 +0000

    --

---
+++ Indexing for Web Search (Assignment 1)/Parsing.py
@@ -0,0 +1,84 @@
+'''URL
+http://csee.essex.ac.uk/staff/udo/index.html
+https://www.essex.ac.uk/departments/computer-science-and-electronic-engineering
+'''
+from bs4 import BeautifulSoup
+import urllib
+import re
+import sys
+import numpy as np
+import pandas as pd
+
+if sys.version_info[0] == 3:
+    from urllib.request import urlopen
+else:
+    from urllib import urlopen
+
+relations = pd.DataFrame(columns=['Page','Location','Word','Vector_representation','Relative_Position','IsNER','InLink','Relations','RelatedWords','General_Rank','Relative_Rank'])
+
+'''Defining URLS'''
+url1 = 'http://csee.essex.ac.uk/staff/udo/index.html'
+url2 = 'https://www.essex.ac.uk/departments/computer-science-and-electronic-engineering'
+
+'''Reading URLs' content'''
+Page1 = urlopen(url1)
+Page2 = urlopen(url2)
+Page1 = BeautifulSoup(Page1)
+Page2 = BeautifulSoup(Page2)
+
+'''Acquiring links and links text'''
+links = []
+for link in Page1.findAll('a', attrs={'href': re.compile("^http://")}):
+    text = re.sub('\s+',' ',link.text)
+    linkObject = [link.get('href'), text]
+    links.append(linkObject)
+    #print(linkObject)
+
+'''Pure text processing after getting links'''
+Page1Text = re.sub('\s+',' ',Page1.text)
+print(Page1Text)
+'''===================
+Sentences tokenization
+===================='''
+import nltk.tokenize.api
+senteces = nltk.tokenize.api.TokenizerI(Page1Text)
+print (senteces)
+'''===================
+Words tokenization
+===================='''
+
+'''===================
+Multi-words tokenization
+===================='''
+
+
+'''===================
+Normalization
+===================='''
+
+'''===================
+POS tagging
+===================='''
+
+'''===================
+Remove stop-words
+===================='''
+
+'''===================
+Bag of words
+===================='''
+
+'''===================
+NER
+===================='''
+
+'''===================
+Index building
+===================='''
+
+
+
+
+
+
+
